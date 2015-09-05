98fbb7b6858b4b852991494f049e667e33a9fc66
vaibhavjain <vaibhavjain882@gmail.com>
2015-09-06 00:17:02 +0530

    Working Model

---
+++ public/controllers/homePageCtrl.js
@@ -0,0 +1,6 @@
+var homePageController = angular.module('HomePageController',[]);
+console.log("inside HomePage COntroller");
+homePageController.controller('homePageCtrl',['$scope','$http', function($scope,$http){
+  
+        };         
+    ]);
