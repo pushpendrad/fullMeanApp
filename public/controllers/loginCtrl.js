d8667cc03c759aad729a8de231bb77a9a81588a3
prakash0.cybage1 <prakashsinhab@cybage.com>
2015-09-11 17:39:21 +0530

    sticky footer from jsfiddle

--- public/controllers/loginCtrl.js
+++ public/controllers/loginCtrl.js
@@ -5,16 +5,5 @@ loginController.controller('loginCtrl',['$scope','$http', function($scope,$http)
 	    $scope.login= function(){
             console.log($scope.user.userName);
             console.log($scope.user.password);
-            $http.get('/alm/register/un').success(function(response){
-                var dBUser = response;
-                    if(dBUser.Password == $scope.password){
-                        redirect to list page
-                }
-                else {
-                redirect to login page again
-            }
-    });   
         };  
     }]);
-
-
